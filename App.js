184db5384797671671d4bae1d2e4216c2e696aa3
Aarvi poudel <aarvi@Aarvis-MacBook-Pro.local>
2020-02-12 13:01:24 +0200

    console log "hello world"

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {MediaContext, MediaProvider} from './contexts/MediaContext';
 import Home from './views/Home.js';
 import AppContainer from './navigators/Navigator.js';
-
+console.log('Hello react')
 const App = () => {
   return (
     <MediaProvider>
